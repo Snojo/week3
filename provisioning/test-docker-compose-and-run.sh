2ab93cab8a24aae531637e068aaed1b3a52b287a
Snojo <snorri13@ru.is>
2017-12-13 13:22:51 +0000

    DIE

--- provisioning/test-docker-compose-and-run.sh
+++ provisioning/test-docker-compose-and-run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-GIT_COMMIT=$(git rev-parse HEAD) > .env
+GIT_COMMIT=$(git rev-parse HEAD)
+DIE = "GIT_COMMIT=${GIT_COMMIT}" > ./.env
 
 /usr/local/bin/docker-compose up -d --build
